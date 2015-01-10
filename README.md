944edbda35afc9f895a1c49db8d9ab3650a351dc
jonjuscorpion666@gmail.com <johnthomas@johns-MBP.lan>
2015-01-10 13:33:02 +1100

    updated sorted list example

---
+++ README.md
@@ -0,0 +1,2 @@
+The project gives basic examples on Case class, Pattern Matching 
+Updated with List 
---
+++ src/com/john/test/sortinglist.scala
@@ -0,0 +1,21 @@
+
+
+package com.john.test
+
+object sortinglist {
+
+ def main(args: Array[String]) {
+  
+println(isort(List(1,3,5,2,44,5,7)))
+  }  
+    def isort(xs: List[Int]) :List[Int]={
+      if(xs.isEmpty) Nil
+      else
+        insert(xs.head,isort(xs.tail))
+   }
+    def insert(x:Int,xs:List[Int]):List[Int]={
+      if(xs.isEmpty || x>=xs.head) x::xs
+      else xs.head :: insert(x,xs.tail) 
+    }
+}
+  
